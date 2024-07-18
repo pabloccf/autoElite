1bdde14a170af68f435e6541ddb3e249da33e0f4
Pablo López Gosálvez <palogo2001@hotmail.es>
2024-07-18 13:04:33 +0200

    WIP

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -83,14 +83,14 @@ class RegistrationFormType extends AbstractType
                     ])
                 ]
             ])
-            ->add('agreeTerms', CheckboxType::class, [
+            /*->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
                         'message' => 'You should agree to our terms.',
                     ]),
                 ],
-            ])
+            ])*/
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller

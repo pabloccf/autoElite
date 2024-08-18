05e00eeab13bfefb5e190b83264bf0be45a8795d
Pablo López Gosálvez <palogo2001@hotmail.es>
2024-08-18 20:05:37 +0200

    Created the view to view a user's data

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use App\Service\UserService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,15 +14,17 @@ class UserController extends AbstractController
 
     private UserService $userService;
     private EntityManagerInterface $entityManager;
+    private UserRepository $userRepository;
 
     /**
      * @param UserService $userService
      * @param EntityManagerInterface $entityManager
      */
-    public function __construct(UserService $userService, EntityManagerInterface $entityManager)
+    public function __construct(UserService $userService, EntityManagerInterface $entityManager, UserRepository $userRepository)
     {
         $this->userService = $userService;
         $this->entityManager = $entityManager;
+        $this->userRepository = $userRepository;
     }
 
     #[Route('/user', name: 'list_user')]
@@ -30,5 +33,17 @@ class UserController extends AbstractController
         $result = $this->userService->getUsers();
 
         return $this->render('user/index.html.twig', array('users' => $result['data']));
+    }
+
+    #[Route('/view/{id}', name: 'view_user')]
+    public function view($id): Response
+    {
+        $user = $this->userRepository->find($id);
+
+        if (!$user) {
+            throw $this->createNotFoundException('Usuario no encontrado');
+        }
+
+        return $this->render('user/view.html.twig', array('user' => $user));
     }
 }
